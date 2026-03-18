c0a21edd1febf5556b99637bef85acfffee39898
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2026-03-19 02:39:15 +0800

    Update platform.sh
    Signed-off-by: kiddin9 <48883331+kiddin9@users.noreply.github.com>

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -132,7 +132,8 @@ platform_do_upgrade() {
 	wallys,dr40x9)
 		nand_do_upgrade "$1"
 		;;
-	alfa-network,ap120c-ac)
+	alfa-network,ap120c-ac|\
+	aliyun,ap4220)
 		part="$(awk -F 'ubi.mtd=' '{printf $2}' /proc/cmdline | sed -e 's/ .*$//')"
 		if [ "$part" = "rootfs1" ]; then
 			fw_setenv active 2 || exit 1
