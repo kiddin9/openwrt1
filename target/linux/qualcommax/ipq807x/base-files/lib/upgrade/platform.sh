da8ec156f008238fab2a25e5fa20970a18b0e929
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2026-08-25 04:13:18 +0800

    Update platform.sh
    Signed-off-by: kiddin9 <48883331+kiddin9@users.noreply.github.com>

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -242,21 +242,29 @@ platform_do_upgrade() {
 	redmi,ax6|\
 	xiaomi,ax3600|\
 	xiaomi,ax9000)
-		# Make sure that UART is enabled
-		fw_setenv boot_wait on
-		fw_setenv uart_en 1
+		part_num="$(fw_printenv -n flag_boot_rootfs)"
+		if [ "$part_num" -eq "1" ]; then
+			CI_UBIPART="rootfs_1"
+			target_num=1
+			# Reset fail flag for the current partition
+			# With both partition set to fail, the partition 2 (bit 1)
+			# is loaded
+			fw_setenv flag_try_sys2_failed 0
+		else
+			CI_UBIPART="rootfs"
+			target_num=0
+			# Reset fail flag for the current partition
+			# or uboot will skip the loading of this partition
+			fw_setenv flag_try_sys1_failed 0
+		fi
 
-		# Enforce single partition.
-		fw_setenv flag_boot_rootfs 0
-		fw_setenv flag_last_success 0
-		fw_setenv flag_boot_success 1
-		fw_setenv flag_try_sys1_failed 8
-		fw_setenv flag_try_sys2_failed 8
+		# Tell uboot to switch partition
+		fw_setenv flag_boot_rootfs "$target_num"
+		fw_setenv flag_last_success "$target_num"
 
-		# Kernel and rootfs are placed in 2 different UBI
-		CI_KERN_UBIPART="ubi_kernel"
-		CI_ROOT_UBIPART="rootfs"
-		CI_DATA_UBIPART="rootfs"
+		Reset success flag
+		fw_setenv flag_boot_success 0
+
 		nand_do_upgrade "$1"
 		;;
 	spectrum,sax1v1k)
